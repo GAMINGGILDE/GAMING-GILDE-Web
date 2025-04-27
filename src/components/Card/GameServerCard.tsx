30417caa2ba4e1e1635b68ec684b1b085cec9816
Julien Obarowski <julienobarowski1@gmail.com>
2025-04-27 12:31:00 +0200

    [FEAT]: bild vergrößert

--- src/components/Card/GameServerCard.tsx
+++ src/components/Card/GameServerCard.tsx
@@ -22,43 +22,45 @@ export const GameServerCard = ({ title, children, cardText, image, onClick, butt
             <div className="my-5">
                 <Link href={"#!"} onClick={() => setShowModal(!isShowModal)}>
                     <div className="card-inner card-inner--blue relative p-4 cursor-pointer text-white">
-                        <div className="flex justify-between items-center mb-3">
+                        <div className="flex justify-between items-center mb-3 lg:hidden block">
                             <h2 className="text-lg lg:text-xl font-semibold uppercase break-words">{title}</h2>
                         </div>
 
-                        <div className="mb-3">
-                           <div className="grid grid-cols-12">
-                               <div className="col-span-12 lg:col-span-6 mx-auto lg:hidden block mb-7">
-                                   <Image
-                                       src={image}
-                                       alt={"gameserver preview"}
-                                       className="max-h-[5rem] object-contain"
-                                       width={100}
-                                       height={100}
-                                       loader={(loader) => loader.src}
-                                   />
+                       <div className="grid grid-cols-12">
+                           <div className="col-span-12 lg:col-span-6 mx-auto lg:hidden block mb-7">
+                               <Image
+                                   src={image}
+                                   alt={"gameserver preview"}
+                                   className="w-[20rem] object-contain rounded-lg"
+                                   width={100}
+                                   height={100}
+                                   loader={(loader) => loader.src}
+                               />
+                           </div>
+                           <div className="col-span-12 lg:col-span-6 text-xm">
+                               <div className="flex justify-between items-center mb-3 lg:block hidden">
+                                   <h2 className="text-lg lg:text-xl font-semibold uppercase break-words">{title}</h2>
                                </div>
-                               <div className="col-span-12 lg:col-span-6 text-xm">
-                                   {cardText}
-                               </div>
-                               <div className="col-span-12 lg:col-span-6 mx-auto lg:block hidden">
-                                   <Image
-                                       src={image}
-                                       alt={"gameserver preview"}
-                                       className="max-h-[5rem] w-[10rem] object-contain rounded-lg"
-                                       width={100}
-                                       height={100}
-                                       loader={(loader) => loader.src}
-                                   />
+
+                               {cardText}
+
+                               <div className="flex mt-3">
+                                   <div onClick={onClick} className="bg-primary px-5 text-center py-0.5 rounded-full flex items-center">
+                                       {button}
+                                   </div>
                                </div>
                            </div>
-                        </div>
-
-                        <div className="flex">
-                            <div onClick={onClick} className="bg-primary px-5 text-center py-0.5 rounded-full flex items-center">
-                                {button}
-                            </div>
-                        </div>
+                           <div className="col-span-12 lg:col-span-6 mx-auto lg:block hidden my-auto">
+                               <Image
+                                   src={image}
+                                   alt={"gameserver preview"}
+                                   className="w-[17.5rem] object-contain rounded-lg"
+                                   width={100}
+                                   height={100}
+                                   loader={(loader) => loader.src}
+                               />
+                           </div>
+                       </div>
 
                     </div>
                 </Link>
