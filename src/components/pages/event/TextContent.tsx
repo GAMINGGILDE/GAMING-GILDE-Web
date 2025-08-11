24ccafa6d1be6eddd34df68223aaf3138219235d
Claus Schiroky <claus@schiroky.com>
2025-08-11 11:57:40 +0200

    - Neuen Event eingepflegt

--- src/components/pages/event/TextContent.tsx
+++ src/components/pages/event/TextContent.tsx
@@ -12,16 +12,93 @@ export const TextContent = () => {
 
     return(
         <div className="my-20 text-white w-[100%] m-auto">
-            {/* <p className="text-xl lg:text-2xl lg:text-4xl font-bold mb-4 uppercase linear-text">LEVEL UP</p> */}
+
+            <p className="text-xl lg:text-2xl lg:text-4xl font-bold mb-4 uppercase linear-text">RETRO, Kult, Legende, Mythos</p>
+
             <p className="text-base lg:text-lg mb-4">
-                <strong>Aktuell steht kein neuer Event an.</strong>
+                In den vergangenen Jahren gab es keinen Event der mehr als alle anderen Veranstaltungen für mehr Furore gesorgt hat als unsere RETRO-Events im Kino. Sie sind Kult und Legende. Damit sie nicht zum Mythos werden, lassen wir ihn einmal mehr aufleben. Das diesjährige Motto:
+            </p>
+
+            <p className="text-base lg:text-lg mb-4">
+                <strong>RETRO, Kult, Legende, Mythos</strong>
+            </p>
+
+            <p className="text-base lg:text-lg mb-4">
+                Es erwartet Euch eine spannende Spieleauswahl an Games unterschiedlichster Genres und Zeitalter. Dabei wird für jeden etwas an Entertainment und Retro-Nostalgie geboten werden. Games die vor 20 Jahren populär waren. Games aus der XBOX 360-Ära. Gespielt wird über die große Leinwand im Kino über eine Microsoft XBOX 360.
+            </p>
+
+            <p className="text-base lg:text-lg mb-4">
+                Obendrein erhält jeder Teilnehmer*in ein limitiertes Überraschungspaket als Erinnerung für den Event.
+            </p>
+
+            <p className="text-base lg:text-lg mb-4">
+                <strong>Wann findet der Event statt?</strong>
             </p>
 
             <p className="text-base lg:text-lg mb-0">
-                Schau gerne später wieder vorbei oder komm auf unseren <a className="text-primary hover:underline" href="https://discord.anti-corona-kartell.de" target="_blank">Discord-Server</a>, um kein Event mehr zu verpassen.
+                Es geht los um 9:00 Uhr am Samstag, den 25. Oktober 2025 im <a className="text-primary hover:underline" href="https://www.cineplex.de/infos/anfahrt-und-oeffnungszeiten/neufahrn/" target="_blank">Cineplex Kino in Neufahrn</a>:
             </p>
             <p className="text-base lg:text-lg mb-4">
-                Ein paar Events die du bereits verpasst hast:
+                Bajuwarenstr. 5, 85375 Neufahrn. Idealerweise seid ihr um 8:45 Uhr am Kino.
+            </p>
+
+            <p className="text-base lg:text-lg mb-4">
+                <strong>Wie lange kann gespielt werden?</strong>
+            </p>
+
+            <p className="text-base lg:text-lg mb-4">
+                Für uns ist ein Kinosaal für 3 Stunden reserviert. Damit endet der Event offiziell um 12:00 Uhr.
+            </p>
+
+            <p className="text-base lg:text-lg mb-4">
+                <strong>Wer kann mitspielen?</strong>
+            </p>
+
+            <p className="text-base lg:text-lg mb-4">
+                Mitspielen und ins Kino eingelassen werden ausschließlich Mitglieder der Discord-Server <a className="text-primary hover:underline" href="https://discord.anti-corona-kartell.de/" target="_blank">ANTI-CORONA-KARTELL</a> und <a className="text-primary hover:underline" href="https://discord.minecraft-gilde.de/" target="_blank">Minecraft Gilde</a>, welche vor dem Einlass einen <a className="text-primary hover:underline" href="https://anti-corona-kartell.de/downloads/haftungsausschluss.pdf" target="_blank">Haftungsausschluss</a> unterschrieben haben. Zur Authentifizierung beim Einlass wird der Discord Profilname herangezogen. Um sich für den Einlass als berechtigt authentifizieren zu können muss die Discord Mobile App auf dem mitgeführten Mobiltelefon funktionsfähig installiert sein. Du musst mind. 16 Jahre alt sein, um mitmachen zu dürfen.
+            </p>
+
+            <p className="text-base lg:text-lg mb-4">
+                <strong>Wie sieht der Ablauf aus?</strong>
+            </p>
+
+            <p className="text-base lg:text-lg mb-4">
+                Treffpunkt ist um 8:45 Uhr vor dem Kinoeingang. Da das Kino erst später öffnet, steht ihr vor verschlossenen Türen, wenn ihr nach 9:00 Uhr ankommt. Zu dem Zeitpunkt ist geplant mit dem Event im Kinosaal zu starten. Um eingelassen zu werden, müsst ihr dann telefonischen Kontakt mit mir oder einem der Teilnehmer aufnehmen, damit jemand ins Foyer geht und euch einlässt. Am einfachsten sollte das über die Discord Mobile App gehen, über die ihr mich oder andere Teilnehmer erreichen könnt. Alternativ findet ihr meine Rufnummer im betreffenden Event in Discord hinterlegt.
+            </p>
+
+            <p className="text-base lg:text-lg mb-4">
+                <strong>Wie sieht der Ablauf aus?</strong>
+            </p>
+
+            <p className="text-base lg:text-lg mb-4">
+                Da der normale Kinobetrieb erst ab 12:30 Uhr startet, sind auch alle Snack-Shops als auch das Kino selbst bis dahin für alle anderen geschlossen. Wer während des Events etwas Essen oder Trinken will, muss sich also Snacks und Getränke bitte selbst mitbringen.
+            </p>
+
+            <p className="text-base lg:text-lg mb-4">
+                <strong>Wie geht’s nach dem Event weiter?</strong>
+            </p>
+
+            <p className="text-base lg:text-lg mb-4">
+                Ihr macht euch auf den Heimweg oder aber ihr kommt noch mit auf ein einzigartig leckeres Mittagessen in der ältesten Brauerei der Welt! Wir begeben uns in die Staatsbrauerei Weihenstephan, mit ihrer jahrhundertealten Geschichte. Sie ist auch die Geburtsstätte einer bayerischen Brotzeitspezialität, dem <a className="text-primary hover:underline" href="https://www.braeustueberl-weihenstephan.de/ueberuns.html#obazde" target="_blank">Obazdn</a>. Als Stammhaus der Brauerei zählt das <a className="text-primary hover:underline" href="https://www.braeustueberl-weihenstephan.de/index.html" target="_blank">Bräustüberl Weihenstephan</a> (Weihenstephaner Berg 10, 85354 Freising) zu den traditionsreichsten Häusern Bayerns und wir werden dort in geselliger Runde das Erlebte gemütlich ausklingen lassen.
+            </p>
+
+            <p className="text-base lg:text-lg mb-4">
+                <strong>Wichtig: </strong>Wenn du beim Essen mit dabei sein willst, musst du dich bitte in Discord über den <a className="text-primary hover:underline" href="https://discord.gg/AzBXMJrF?event=1404402385027334275" target="_blank">betreffenden Event</a> als „Interessiert“ anmelden, damit wir bei der Reservierung genügend Sitzplätze gewährleisten können.
+            </p>
+
+            <p className="text-base lg:text-lg mb-12">
+                Bis dahin: Happy Gaming!
+            </p>
+
+
+
+
+            {/* <p className="text-base lg:text-lg mb-0">
+                Schau gerne später wieder vorbei oder komm auf unseren <a className="text-primary hover:underline" href="https://discord.anti-corona-kartell.de" target="_blank">Discord-Server</a>, um kein Event mehr zu verpassen.
+            </p>  */}
+
+            <p className="text-base lg:text-lg mb-4">
+                <strong>VERGANGENE EVENTS:</strong>
             </p>
 
             <p className="text-base lg:text-lg mb-0">
@@ -53,7 +130,7 @@ export const TextContent = () => {
             </p>
 
             <p className="text-base lg:text-lg mb-4">
-                Wenn du wissen willst, was du sonst noch so verpasst hast, dann schau einfach mal bei unserem <a className="text-primary hover:underline" href="https://www.youtube.com/@Anti-Corona-Kartell/videos" target="_blank">YouTube-Kanal</a> vorbei 😎.
+                Schau dir auch gerne ein paar Videos vergangener Events auf unserem <a className="text-primary hover:underline" href="https://www.youtube.com/@Anti-Corona-Kartell/videos" target="_blank">YouTube-Kanal</a> an 😎.
             </p>
 
             {/* <div className="flex justify-between w-full items-center mt-10 mb-10">
