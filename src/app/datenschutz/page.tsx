dd2829837b716b6bf1f7154d0e731f486dc29fac
Claus Schiroky <claus@schiroky.com>
2026-01-23 20:54:16 +0100

    - Datenschutzerklärung aktualisiert.

--- src/app/datenschutz/page.tsx
+++ src/app/datenschutz/page.tsx
@@ -56,42 +56,16 @@ export default function Datenschutz() {
                 <div className="space-y-2">
                     <h2 className="text-2xs font-semibold">Cookies</h2>
                     <p className="sub-text font-light">
-                        Wie viele andere Webseiten verwenden wir auch so genannte „Cookies“, um das Internetangebot
-                        nutzerfreundlicher, effektiver und sicherer zu gestalten. Cookies sind kleine Textdateien. Sie
-                        unterstützen die Darstellung unserer Webseite und helfen Ihnen, sich auf unserer Webseite zu
-                        bewegen. Cookies erfassen Daten zu Ihrer IP-Adresse, zu Ihrem Browser, Ihrem Betriebssystem und
-                        Ihrer Internetverbindung. Diese Informationen verbinden wir nicht mit personenbezogenen Daten
-                        und geben sie nicht an Dritte weiter. Sie können unsere Webseite auch ohne den Einsatz von
-                        Cookies nutzen, wodurch möglicherweise einige Darstellungen und Funktionen unseres Angebots nur
-                        eingeschränkt arbeiten. Diese werden zur technischen Sitzungssteuerung im temporären Speicher
-                        des Webbrowsers bzw. auf der Festplatte des Besuchers der Internetseite abgelegt.
+                        Unsere Website ist überwiegend statisch aufgebaut und setzt selbst keine eigenen Tracking- oder Analyse-Cookies ein. Durch uns als Websitebetreiber findet keine Erstellung personenbezogener Nutzungsprofile statt.
                     </p>
                     <p className="sub-text font-light">
-                        Unter anderem speichern wir auf Ihrer Festplatte so genannte Session-Cookies, die nur für die
-                        Dauer Ihres Besuches auf unserer Webseite Gültigkeit besitzen. Diese werden benötigt, um Sie
-                        nach erfolgreicher Anmeldung im geschützten Bereich für die gesamte Dauer Ihres Besuches zu
-                        identifizieren und zu autorisieren. Nach Ende Ihres Besuches werden diese Cookies automatisch
-                        von Ihrer Festplatte wieder gelöscht.
+                        Ihr Browser kann einzelne Inhalte wie Bilder, Stylesheets oder Skripte im Cache zwischenspeichern, um Ladezeiten zu verbessern. Dabei handelt es sich nicht um klassische Cookies, sondern um eine normale technische Funktion Ihres Browsers.
                     </p>
                     <p className="sub-text font-light">
-                        Das Speichern von Cookies können Besucher von Internetseiten durch bestimmte
-                        Browser-Einstellungen verhindern. In der Regel lässt sich der Webbrowser auch so einstellen,
-                        dass dieser beim Besucher der Internetseite vor dem Setzen von Cookies nachfragt, ob er
-                        einverstanden ist.
-                        Ferner können einmal gesetzte Cookies jederzeit wieder vom Besucher der Internetseite gelöscht
-                        werden. Die Produktbeschreibungen/Anleitungen der jeweiligen Browser-Hersteller enthalten
-                        Informationen zu den hierzu erforderlichen Einstellungen. Bitte beachten Sie, dass einzelne
-                        Funktionen unserer Webseite möglicherweise nicht funktionieren, wenn Sie die Verwendung von
-                        Cookies deaktiviert haben.
+                        Auf einzelnen Unterseiten binden wir externe Inhalte ein, insbesondere Videos der Plattform YouTube. Beim Aufruf oder spätestens beim Abspielen solcher eingebetteten Videos kann YouTube Cookies oder ähnliche Technologien setzen, auf die wir keinen Einfluss haben. Weitere Informationen hierzu finden Sie im Abschnitt „Einbindung von YouTube-Videos“ dieser Datenschutzerklärung.
                     </p>
                     <p className="sub-text font-light">
-                        Cookies können nicht verwendet werden, um Programme zu starten oder Viren auf einen Computer zu
-                        übertragen. Anhand der in Cookies enthaltenen Informationen können wir Ihnen die Navigation
-                        erleichtern und die korrekte Anzeige unserer Webseiten ermöglichen.
-                    </p>
-                    <p className="sub-text font-light">
-                        In keinem Fall werden die von uns erfassten Daten an Dritte weitergegeben oder ohne Ihre
-                        Einwilligung eine Verknüpfung mit personenbezogenen Daten hergestellt.
+                        Sie können Ihren Browser so einstellen, dass Sie über das Setzen von Cookies informiert werden, Cookies nur im Einzelfall erlauben, die Annahme von Cookies ausschließen oder das automatische Löschen von Cookies beim Schließen des Browsers aktivieren. Bei der Deaktivierung von Cookies kann die Funktionalität anderer Websites eingeschränkt sein, jedoch hat dies in der Regel keine Auswirkungen auf unsere Website.
                     </p>
                 </div>
 
@@ -142,29 +116,7 @@ export default function Datenschutz() {
                 <div className="space-y-2">
                     <h2 className="text-2xs font-semibold">Onlinepräsenzen</h2>
                     <p className="sub-text font-light">
-                        Der Websitebetreiber unterhält auch Onlinepräsenzen innerhalb sozialer Netzwerke und
-                        Plattformen, um mit den dort aktiven Nutzern zu kommunizieren. Ich weise darauf hin, dass dabei
-                        Daten der Nutzer außerhalb des Raumes der Europäischen Union verarbeitet werden können.
-                        Hierdurch können sich für die Nutzer Risiken ergeben, weil so z. B. die Durchsetzung der Rechte
-                        der Nutzer erschwert werden könnte. Im Hinblick auf US-Anbieter die unter dem Privacy-Shield
-                        zertifiziert sind, weise ich darauf hin, dass sie sich damit verpflichten, die
-                        Datenschutzstandards der EU einzuhalten. Ferner werden die Daten der Nutzer im Regelfall für
-                        Marktforschungs- und Werbezwecke verarbeitet. So können z. B. aus dem Nutzungsverhalten und sich
-                        daraus ergebenden Interessen der Nutzer Nutzungsprofile erstellt werden. Die Nutzungsprofile
-                        können wiederum verwendet werden, um z. B. Werbeanzeigen innerhalb und außerhalb der Plattformen
-                        zu schalten, die mutmaßlich den Interessen der Nutzer entsprechen. Zu diesen Zwecken werden im
-                        Regelfall Cookies auf den Rechnern der Nutzer gespeichert, in denen das Nutzungsverhalten und
-                        die Interessen der Nutzer gespeichert werden. Ferner können in den Nutzungsprofilen auch Daten
-                        unabhängig der von den Nutzern verwendeten Geräte gespeichert werden (insbesondere, wenn die
-                        Nutzer Mitglieder der jeweiligen Plattformen sind und bei diesen eingeloggt sind). Die
-                        Verarbeitung der personenbezogenen Daten der Nutzer erfolgt auf Grundlage meinem berechtigten
-                        Interesse an einer effektiven Information der Nutzer und Kommunikation mit den Nutzern gem. Art.
-                        6 Abs. 1 lit. f. DSGVO. Falls die Nutzer von den jeweiligen Anbietern um eine Einwilligung in
-                        die Datenverarbeitung gebeten werden (d. h. ihr Einverständnis z. B. über das Anhaken eines
-                        Kontrollkästchens oder Bestätigung einer Schaltfläche erklären) ist die Rechtsgrundlage der
-                        Verarbeitung Art. 6 Abs. 1 lit. a., Art. 7 DSGVO. Für eine detaillierte Darstellung der
-                        jeweiligen Verarbeitungen, verweise ich auf die nachfolgend verlinkten Datenschutzerklärungen
-                        der Anbieter:
+                        Wir unterhalten auch Onlinepräsenzen innerhalb sozialer Netzwerke und Plattformen, um mit den dort aktiven Nutzern zu kommunizieren. Wir weisen darauf hin, dass dabei Daten der Nutzer außerhalb des Raumes der Europäischen Union verarbeitet werden können. Hierdurch können sich für die Nutzer Risiken ergeben, weil so z. B. die Durchsetzung der Rechte der Nutzer erschwert werden könnte. Soweit Anbieter ihren Sitz in Drittländern (insbesondere den USA) haben, weisen wir darauf hin, dass dort ggf. kein mit der EU vergleichbares Datenschutzniveau besteht. Eine Datenübermittlung erfolgt nur, soweit sie nach Kapitel V DSGVO zulässig ist (z. B. auf Grundlage von Standardvertragsklauseln oder eines Angemessenheitsbeschlusses). Ferner werden die Daten der Nutzer im Regelfall für Marktforschungs- und Werbezwecke verarbeitet. So können z. B. aus dem Nutzungsverhalten und sich daraus ergebenden Interessen der Nutzer Nutzungsprofile erstellt werden. Die Nutzungsprofile können wiederum verwendet werden, um z. B. Werbeanzeigen innerhalb und außerhalb der Plattformen zu schalten, die mutmaßlich den Interessen der Nutzer entsprechen. Zu diesen Zwecken werden im Regelfall Cookies auf den Rechnern der Nutzer gespeichert, in denen das Nutzungsverhalten und die Interessen der Nutzer gespeichert werden. Ferner können in den Nutzungsprofilen auch Daten unabhängig der von den Nutzern verwendeten Geräte gespeichert werden (insbesondere, wenn die Nutzer Mitglieder der jeweiligen Plattformen sind und bei diesen eingeloggt sind). Die Verarbeitung der personenbezogenen Daten der Nutzer erfolgt auf Grundlage unseres berechtigten Interesses an einer effektiven Information der Nutzer und Kommunikation mit den Nutzern gem. Art. 6 Abs. 1 lit. f. DSGVO. Falls die Nutzer von den jeweiligen Anbietern um eine Einwilligung in die Datenverarbeitung gebeten werden (d. h. ihr Einverständnis z. B. über das Anhaken eines Kontrollkästchens oder Bestätigung einer Schaltfläche erklären), ist die Rechtsgrundlage der Verarbeitung Art. 6 Abs. 1 lit. a., Art. 7 DSGVO. Für eine detaillierte Darstellung der jeweiligen Verarbeitungen verweisen wir auf die nachfolgend verlinkten Datenschutzerklärungen der Anbieter:
                     </p>
                     <ul className="list-disc list-inside m-4">
                         <li><a className="font-light text-primary hover:underline" href="https://policies.google.com/privacy?hl=de"
@@ -187,60 +139,24 @@ export default function Datenschutz() {
 
                 {/* Paragraph 8 */}
                 <div className="space-y-2">
-                    <h2 className="text-2xs font-semibold">Verwendung von Scriptbibliotheken (Google Webfonts)</h2>
+                    <h2 className="text-2xs font-semibold">Erfassung und Verarbeitung personenbezogener Daten</h2>
                     <p className="sub-text font-light">
-                        Um unsere Inhalte browserübergreifend korrekt und grafisch ansprechend darzustellen, verwenden
-                        wir auf dieser Website „Google Web Fonts“ der Google LLC (1600 Amphitheatre Parkway, Mountain
-                        View, CA 94043, USA; nachfolgend „Google“) zur Darstellung von Schriften. Weitere Informationen zu Google Web Fonts finden Sie <a className="text-primary hover:underline" href="https://developers.google.com/fonts/faq" target="_blank">hier</a>.
+                        Wir erheben, nutzen und geben Ihre personenbezogenen Daten nur dann weiter, wenn dies im gesetzlichen Rahmen erlaubt ist oder Sie in die Datenerhebung einwilligen. Als personenbezogene Daten gelten sämtliche Informationen, welche dazu dienen, Ihre Person zu bestimmen und welche zu Ihnen zurückverfolgt werden können – also beispielsweise Ihr Name, Ihre E-Mail-Adresse oder Telefonnummer. Diese Website können Sie auch besuchen, ohne Angaben zu Ihrer Person zu machen.
+                    </p>
+                    <p className="sub-text font-light">
+                        Ergänzend zu den unter „Zugriffsdaten“ beschriebenen technischen Logdaten verarbeiten wir personenbezogene Daten nur nach ausdrücklicher Erlaubnis der betreffenden Nutzer und unter Einhaltung der geltenden Datenschutzbestimmungen. Die Verarbeitung der personenbezogenen Daten erfolgt insbesondere zur Bearbeitung von Anfragen, zur Wahrung unserer berechtigten Interessen sowie – soweit einschlägig – zur Erfüllung vertraglicher oder vorvertraglicher Pflichten.
                     </p>
                 </div>
 
                 {/* Paragraph 9 */}
                 <div className="space-y-2">
-                    <h2 className="text-2xs font-semibold">Erfassung und Verarbeitung personenbezogener Daten</h2>
+                    <h2 className="text-2xa font-semibold">Umgang mit Kontaktdaten</h2>
                     <p className="sub-text font-light">
-                        Der Websitebetreiber erhebt, nutzt und gibt Ihre personenbezogenen Daten nur dann weiter, wenn
-                        dies im gesetzlichen Rahmen erlaubt ist oder Sie in die Datenerhebung einwilligen. Als
-                        personenbezogene Daten gelten sämtliche Informationen, welche dazu dienen, Ihre Person zu
-                        bestimmen und welche zu Ihnen zurückverfolgt werden können – also beispielsweise Ihr Name, Ihre
-                        E-Mail-Adresse oder Telefonnummer. Diese Website können Sie auch besuchen, ohne Angaben zu Ihrer
-                        Person zu machen. Zur Verbesserung unseres Online-Angebotes speichert jedoch unter
-                        Hosting-Anbieter (ohne Personenbezug) Ihre Zugriffsdaten auf diese Website. Zu diesen
-                        Zugriffsdaten gehören die von Ihnen angeforderte Datei und das Zugriffsdatum. Durch die
-                        Anonymisierung der Daten sind Rückschlüsse auf Ihre Person nicht möglich. Wir verarbeiten
-                        personenbezogene Daten nur nach ausdrücklicher Erlaubnis der betreffenden Nutzer und unter
-                        Einhaltung der geltenden Datenschutzbestimmungen. Die Verarbeitung der personenbezogenen Daten
-                        erfolgt aufgrund unseres berechtigten Interesses zur Erfüllung unserer vertraglich vereinbarten
-                        Leistungen und zur Optimierung unseres Online-Angebotes.
+                        Nehmen Sie mit uns als Websitebetreiber über die angebotenen Kontaktmöglichkeiten (z. B. per E-Mail) Verbindung auf, werden Ihre Angaben gespeichert, damit auf diese zur Bearbeitung und Beantwortung Ihrer Anfrage zurückgegriffen werden kann. Ohne Ihre Einwilligung werden diese Daten nicht an Dritte weitergegeben. Ein gesondertes Kontaktformular wird nicht angeboten.
                     </p>
                 </div>
 
                 {/* Paragraph 10 */}
-                <div className="space-y-2">
-                    <h2 className="text-2xa font-semibold">Kontaktformular</h2>
-                    <p className="sub-text font-light">
-                        Treten Sie bzgl. Fragen jeglicher Art per E-Mail oder Kontaktformular mit uns in Kontakt,
-                        erteilen Sie uns zum Zwecke der Kontaktaufnahme Ihre freiwillige Einwilligung. Hierfür ist die
-                        Angabe einer validen E-Mail-Adresse erforderlich. Diese dient der Zuordnung der Anfrage und der
-                        anschließenden Beantwortung derselben. Die Angabe weiterer Daten ist optional. Die von Ihnen
-                        gemachten Angaben werden zum Zwecke der Bearbeitung der Anfrage sowie für mögliche
-                        Anschlussfragen gespeichert. Nach Erledigung der von Ihnen gestellten Anfrage werden
-                        personenbezogene Daten automatisch gelöscht.
-                    </p>
-                </div>
-
-                {/* Paragraph 11 */}
-                <div className="space-y-2">
-                    <h2 className="text-2xs font-semibold">Umgang mit Kontaktdaten</h2>
-                    <p className="sub-text font-light">
-                        Nehmen Sie mit uns als Websitebetreiber durch die angebotenen Kontaktmöglichkeiten Verbindung
-                        auf, werden Ihre Angaben gespeichert, damit auf diese zur Bearbeitung und Beantwortung Ihrer
-                        Anfrage zurückgegriffen werden kann. Ohne Ihre Einwilligung werden diese Daten nicht an Dritte
-                        weitergegeben.
-                    </p>
-                </div>
-
-                {/* Paragraph 12 */}
                 <div className="space-y-2">
                     <h2 className="text-2xs font-semibold">Rechte des Nutzers</h2>
                     <p className="sub-text font-light">
@@ -253,7 +169,7 @@ export default function Datenschutz() {
                     </p>
                 </div>
 
-                {/* Paragraph 13 */}
+                {/* Paragraph 11 */}
                 <div className="space-y-2">
                     <h2 className="text-2xs font-semibold">Löschung von Daten</h2>
                     <p className="sub-text font-light">
@@ -267,7 +183,7 @@ export default function Datenschutz() {
                     </p>
                 </div>
 
-                {/* Paragraph 14 */}
+                {/* Paragraph 12 */}
                 <div className="space-y-2">
                     <h2 className="text-2xs font-semibold">Änderung unserer Datenschutzbestimmungen</h2>
                     <p className="sub-text font-light">
@@ -278,7 +194,7 @@ export default function Datenschutz() {
                     </p>
                 </div>
 
-                {/* Paragraph 15 */}
+                {/* Paragraph 13 */}
                 <div className="space-y-2">
                     <h2 className="text-2xs font-semibold">Widerspruchsrecht</h2>
                     <p className="sub-text font-light">
@@ -294,7 +210,7 @@ export default function Datenschutz() {
                 {/* Footer */}
                 <footer className="border-t border-gray-700 pt-6">
                     <p className="text-sm text-gray-400">
-                        Stand: März 2025 &ndash; Änderungen und Irrtümer vorbehalten.
+                        Stand: Januar 2026.
                     </p>
                 </footer>
             </div>
