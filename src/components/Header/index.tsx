7e50eb7f7d79ff783607a0ba8cf2620ce90ae27a
Claus Schiroky <claus@schiroky.com>
2026-02-19 14:38:51 +0100

    - Startseiten-Texte modernisiert.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -62,7 +62,7 @@ export const Header = ({
             </p>
           )
         }
-        text={noText ? <></> : <>Eine junge Gaming-Community, für gemeinsamen Spaß beim Spielen</>}
+        text={noText ? <></> : <>Eine junge Gaming-Community, für gemeinsamen Spaß beim Zocken</>}
       ></Headline>
     </>
   );
