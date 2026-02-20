8c41c2b599ced889cfd9b360a01ecf033b621b4b
Claus Schiroky <claus@schiroky.com>
2026-02-20 17:38:46 +0100

    Prettier für Footer umgesetzt.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -13,72 +13,74 @@ import {
 
 export const Footer = () => {
   return (
-      <div>
-        <div className="mt-5 text-white container m-auto px-10">
-          <div className="my-24">
-            <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between flex-wrap font-extralight text-xs">
-              <p className="lg:flex-1 text-left">
-                &copy;{new Date().getFullYear()}{" "}
-                <a href="https://schiroky.com" target="_blank">
-                  Claus Schiroky </a>
-                . Alle Rechte vorbehalten.
-              </p>
+    <div>
+      <div className="mt-5 text-white container m-auto px-10">
+        <div className="my-24">
+          <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between flex-wrap font-extralight text-xs">
+            <p className="lg:flex-1 text-left">
+              &copy;{new Date().getFullYear()}{" "}
+              <a href="https://schiroky.com" target="_blank">
+                Claus Schiroky{" "}
+              </a>
+              . Alle Rechte vorbehalten.
+            </p>
 
-              <div className="mt-3 lg:mt-0 flex justify-center items-center lg:flex-1">
-                <div className="mx-2">
-                  <Link href="https://discord.anti-corona-kartell.de" title="Discord" target="_blank">
-                    <FaDiscord className="w-5 h-5 text-white hover:text-primary" />
-                  </Link>
-                </div>
-
-                <div className="mx-2">
-                  <Link href="https://www.tiktok.com/@anticoronakartell"
-                        title="TikTok"
-                        target="_blank"
-                  >
-                    <FaTiktok className="w-5 h-5 text-white hover:text-primary" />
-                  </Link>
-                </div>
-
-                <div className="mx-2">
-                  <Link href="https://www.twitch.tv/anti_corona_kartell"
-                        title="Twitch"
-                        target="_blank"
-                  >
-                    <FaTwitch className="w-5 h-5 text-white hover:text-primary" />
-                  </Link>
-                </div>
-
-                <div className="mx-2">
-                  <Link href="https://www.youtube.com/@Anti-Corona-Kartell"
-                        title="YouTube"
-                        target="_blank"
-                  >
-                    <FaYoutube className="w-5 h-5 text-white hover:text-primary" />
-                  </Link>
-                </div>
-
-                <div className="mx-2">
-                  <Link href="https://github.com/ANTI-CORONA-KARTELL" title="GitHub" target="_blank">
-                    <FaGithub className="w-5 h-5 text-white hover:text-primary" />
-                  </Link>
-                </div>
+            <div className="mt-3 lg:mt-0 flex justify-center items-center lg:flex-1">
+              <div className="mx-2">
+                <Link href="https://discord.anti-corona-kartell.de" title="Discord" target="_blank">
+                  <FaDiscord className="w-5 h-5 text-white hover:text-primary" />
+                </Link>
               </div>
 
-              <div className="mt-3 lg:mt-0 lg:flex-1 text-right">
-
-                <Link href="/impressum" title="Impressum" className="hover:text-primary">
-                  Impressum </Link>
-
-                <Link href="/datenschutz" title="Datenschutz" className="hover:text-primary">
-                  Datenschutz </Link>
-
+              <div className="mx-2">
+                <Link
+                  href="https://www.tiktok.com/@anticoronakartell"
+                  title="TikTok"
+                  target="_blank"
+                >
+                  <FaTiktok className="w-5 h-5 text-white hover:text-primary" />
+                </Link>
               </div>
 
+              <div className="mx-2">
+                <Link
+                  href="https://www.twitch.tv/anti_corona_kartell"
+                  title="Twitch"
+                  target="_blank"
+                >
+                  <FaTwitch className="w-5 h-5 text-white hover:text-primary" />
+                </Link>
+              </div>
+
+              <div className="mx-2">
+                <Link
+                  href="https://www.youtube.com/@Anti-Corona-Kartell"
+                  title="YouTube"
+                  target="_blank"
+                >
+                  <FaYoutube className="w-5 h-5 text-white hover:text-primary" />
+                </Link>
+              </div>
+
+              <div className="mx-2">
+                <Link href="https://github.com/ANTI-CORONA-KARTELL" title="GitHub" target="_blank">
+                  <FaGithub className="w-5 h-5 text-white hover:text-primary" />
+                </Link>
+              </div>
+            </div>
+
+            <div className="mt-3 lg:mt-0 lg:flex-1 text-right">
+              <Link href="/impressum" title="Impressum" className="hover:text-primary">
+                Impressum{" "}
+              </Link>
+
+              <Link href="/datenschutz" title="Datenschutz" className="hover:text-primary">
+                Datenschutz{" "}
+              </Link>
             </div>
           </div>
         </div>
       </div>
+    </div>
   );
-
 };
